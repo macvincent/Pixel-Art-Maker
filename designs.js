34eaeb2eb8c65edf6877d7db8ae995636f00e343
MacVincent <macvincentaghaoko@gmail.com>
2019-01-18 10:16:51 -0500

    Modifies the .js file to create the pixel-art maker

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+//for color selection
+let color = $('#colorPicker').val();
+$('#colorPicker').on('change',function(){
+	color = $('#colorPicker').val();
+});
+//manages the making of the grid
+function makeGrid(evt){
+	evt.preventDefault();
+	let td;
+    let tr;
+	td = $('#inputHeight').val();
+	tr = $("#inputWidth").val();
+	let row = "<tr>";
+	for(let i = 0; i < tr; i++){
+		row+="<td></td>";
+	}
+	row+="</tr>";
+	let height = "";
+	for(let i = 0; i<td; i++){
+		$('table').append(row);
+	}
 }
+//changes color of a grid
+let change = function(evt) {
+	console.log(color);
+	$(evt.target).css('background-color',color);
+};
+
+$('#tab').on('click',makeGrid);
+
+$('table').on('click',change);
